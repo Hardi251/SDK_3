c1eff30f97742f779195bfd7dc448155d3e9d6cf
Hardi251 <hardi.patel@volansys.com>
2023-04-10 14:37:19 +0530

    Framework added

---
+++ SDK_3/SDK_3/SDKService_3.swift
@@ -0,0 +1,17 @@
+//
+//  SDKService_3.swift
+//  SDK_3
+//
+//  Created by Hardi Patel on 10/04/23.
+//
+
+import Foundation
+
+public class SDKService_3 {
+    
+    public init() {}
+    
+    public static func randomString() -> String {
+        return "sDK_3 demo string..."
+    }
+}
---
+++ SDK_3/SDK_3/SDK_3.h
@@ -0,0 +1,18 @@
+//
+//  SDK_3.h
+//  SDK_3
+//
+//  Created by Hardi Patel on 10/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDK_3.
+FOUNDATION_EXPORT double SDK_3VersionNumber;
+
+//! Project version string for SDK_3.
+FOUNDATION_EXPORT const unsigned char SDK_3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDK_3/PublicHeader.h>
+
+
