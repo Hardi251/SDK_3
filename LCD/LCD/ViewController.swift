c1984c8a34aa8aa1f9555941b59b32fb16a5be17
Hardi251 <hardi.patel@volansys.com>
2023-04-10 15:25:18 +0530

    update new example

---
+++ LCD/LCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LCD
+//
+//  Created by Hardi Patel on 10/04/23.
+//
+
+import UIKit
+import SDK_3
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
